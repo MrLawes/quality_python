d55ae96e79bae1307677ed898aeb51b23b7718ad
lawes <haiou_chen@sina.cn>
2020-07-09 09:02:40 +0800

    5.通过适当添加空行使代码布局更为优雅、合理

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-from collections import defaultdict
-s = [('yellow', 1), ('blue', 2), ('yellow', 3), ('blue', 4), ('red', 1)]
-d = defaultdict(list)
-[d[k].append(v) for k, v in s]
-print(d.items())
+import datetime
+a = datetime.datetime.now()
 
+import simplejson as json
+
+json.dumps(a)
